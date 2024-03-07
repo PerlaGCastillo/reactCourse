0b7f8c5c06ec57fabb9f817481e4185cd0d0e2f0
PerlaGCastillo <contactoperlagodinez@gmail.com>
2024-03-07 14:50:45 -0600

    destructuring objects

--- destructuringobjects.js
+++ destructuringobjects.js
@@ -6,6 +6,25 @@ const mascota = {
     clase: 'AAA'
 }
 // const nombreMascota = mascota.nombre
-
 const {nombre, raza} = mascota
 console.log(nombre, raza)
+
+
+const web = {
+    nombre: 'google',
+    links:  {
+        global: 'https://www.google.com/',
+        pais: {
+            mexico: 'https://www.google.com.mx/',
+            japon: 'https://www.google.co.jp/',
+            alemania: 'https://www.google.co.de/',
+
+        }
+    },
+    creadores: {
+        serguei: 'Serguéi Brin',
+        larry: 'Larry Page'
+    }
+}
+
+console.log(web.links.pais.alemania)
