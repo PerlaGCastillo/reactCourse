9610143d8a344a62e64e9458dec42408fa3c5ac4
PerlaGCastillo <contactoperlagodinez@gmail.com>
2024-03-07 17:13:08 -0600

    fetch

---
+++ fetch.js
@@ -0,0 +1,12 @@
+//fetch
+// consumir RESTful API then espera una promesa
+
+fetch("https://pokeapi.co/api/v2/pokemon")
+  .then((res) => res.json())
+  .then((data) => {
+    // console.log(data.results)
+    data.results.forEach((element) => {
+      console.log(element.name)
+    })
+  })
+  .catch((error) => console.log(error))
